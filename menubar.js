cc9e2290bd0830dd89afc3bc078dc2ecec518135
minkokolinn <minkokolinndev@gmail.com>
2024-11-07 15:23:56 +0700

    completed

---
+++ menubar.js
@@ -0,0 +1,5 @@
+function toggleSidebar(){
+	document.getElementById("sidebar").classList.toggle('active');
+	document.getElementById("toggle-btn").classList.toggle('active');
+	document.getElementById("toggle-btnb").classList.toggle('active');
+}
